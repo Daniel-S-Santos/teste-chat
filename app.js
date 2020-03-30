ba191dee9b504da8136b8d2158eb0d4806884a0f
Sistemas GrupoABL <sistemas@gabl.lan>
2020-03-29 23:56:24 -0300

    Init commit

---
+++ app.js
@@ -0,0 +1,31 @@
+var app = require('express')()
+var bodyparser = require('body-parser')
+var server = require('http').Server(app);
+var io = require('socket.io')(server);
+var cors = require('cors')
+app.use(bodyparser.json())
+app.use(bodyparser.urlencoded({extended: true}))
+app.use(cors())
+
+app.get('/', function(req , res){
+    res.sendFile(__dirname+ '/index.html') 
+})
+
+const port = process.env.PORT || 3000
+io.on('connection', function(socket){
+    console.log('a user connected');
+
+    socket.on('chat message', function(msg){
+        io.emit('chat message', msg);
+        console.log('message: ' + msg);        
+      });
+
+    socket.on('disconnect', function(){
+      console.log('user disconnected');
+    });
+  });
+
+server.listen(port, function(){
+    console.log('listening on *: '+port);
+});
+
